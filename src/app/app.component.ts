d310c01a38a0c12877d0523ed875525fdfbd6c57
Geoff Mnr <97231176+Geoff-Mnr@users.noreply.github.com>
2024-02-08 21:24:15 +0100

    update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule, FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'movies';
+
+  fb = inject(FormBuilder);
+
+  form = this.fb.group({
+    title: [{ value: '', disabled: true}, Validators.required],
+    year: [{ value: '', disabled: true}, Validators.min(1885)],
+    director: [{ value: '', disabled: true}, Validators.required],
+    synopsis: [{ value: '', disabled: true}, Validators.required],
+  });
+
 }
